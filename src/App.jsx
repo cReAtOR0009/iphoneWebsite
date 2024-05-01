1697a47fde047ac9e341b11fa7a9ee7a807d9677
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-05-01 09:45:07 +0100

    imported completed component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
+import Features from './components/Features.jsx'
+import HowItWorks from './components/How_works.jsx'
+import Footer from '../../iphone/src/components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +16,9 @@ function App() {
     <Hero/>
     <Highlights/>
     <Model/>
+    <HowItWorks/>
+    <Features/>
+    <Footer/>
    </main>
   )
 }
